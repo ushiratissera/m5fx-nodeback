02b2fd4ff9e2ce33943a0152eab481ca1788d02e
Ushira Tissera <113992253+ushiratissera@users.noreply.github.com>
2022-09-21 12:03:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,19 +11,19 @@ const PORT = process.env.PORT || 8070;
 app.use(cors());
 app.use(bodyParser.json());
 
-const URL = process.env.MONGODB_URL;
+// const URL = process.env.MONGODB_URL;
 
-mongoose.connect(URL,{
-    //useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    //useFindAndModify: false,
-})
+// mongoose.connect(URL,{
+//     //useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     //useFindAndModify: false,
+// })
 
-const connection = mongoose.connection;
-connection.once("open", () => {
-    console.log("Mongo DB Succesfully Connected");
-})
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//     console.log("Mongo DB Succesfully Connected");
+// })
 
 
 // Routes Config
