e2c759f84e22e3dbe83776cb80b3722a0c5fe0bb
Ushira Tissera <113992253+ushiratissera@users.noreply.github.com>
2022-09-21 11:32:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8070;
 
 app.use(cors());
 app.use(bodyParser.json());
